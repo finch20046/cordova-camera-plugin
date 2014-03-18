d2f801fc4091a0402935609bad30ffa77972991e
Marcello Cardea <marcello.cardea@axant.it>
2014-03-18 16:57:03 +0100

    added

--- src/ios/CameraPlugin.h
+++ src/ios/CameraPlugin.h
@@ -1,18 +1,15 @@
 // Note that Xcode gets this line wrong.  You need to change "Cordova.h" to "CDV.h" as shown below.
 #import <Cordova/CDV.h>
 
-// Import the CustomCameraViewController class
-#import "CustomCameraViewController.h"
-
-@interface CameraPlugin : CDVPlugin
+@interface CameraPlugin : CDVPlugin 
 
 // Cordova command method
 -(void) openCamera:(CDVInvokedUrlCommand*)command;
 
 // Create and override some properties and methods (these will be explained later)
 -(void) capturedImageWithPath:(NSString*)imagePath;
-@property (strong, nonatomic) CustomCameraViewController *overlay;
 @property (strong, nonatomic) CDVInvokedUrlCommand *latestCommand;
 @property (readwrite, assign) BOOL hasPendingOperation;
+@property (strong, nonatomic) UIImagePickerController* picker;
 
 @end
--- src/ios/CameraPlugin.m
+++ src/ios/CameraPlugin.m
@@ -1,5 +1,5 @@
 #import "CameraPlugin.h"
-
+#import "AppDelegate.h"
 @implementation CameraPlugin
 
 // Cordova command method
@@ -12,22 +12,57 @@
 	self.latestCommand = command;
     
 	// Make the overlay view controller.
-	self.overlay = [[CustomCameraViewController alloc] initWithNibName:@"CustomCameraViewController" bundle:nil];
-	self.overlay.plugin = self;
+    self.picker = [[UIImagePickerController alloc] init];
+    self.picker.cameraCaptureMode = UIImagePickerControllerCameraCaptureModePhoto;
+    self.picker.cameraDevice = UIImagePickerControllerCameraDeviceRear;
     
+    AppDelegate *delegate = (AppDelegate*)[UIApplication sharedApplication].delegate;
+    UIViewController *rootViewController = delegate.window.rootViewController;
+    
+    [rootViewController presentModalViewController:self.picker animated:YES];
 	// Display the view.  This will "slide up" a modal view from the bottom of the screen.
-	[self.viewController presentViewController:self.overlay.picker animated:YES completion:nil];
+	//[self.viewController presentViewController:self.overlay.picker animated:YES completion:nil];
+    self.picker.delegate = self;
 }
 
-// Method called by the overlay when the image is ready to be sent back to the web view
--(void) capturedImageWithPath:(NSString*)imagePath {
+-(void) imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+    
+	// Get a reference to the captured image
+	UIImage* image = [info objectForKey:UIImagePickerControllerOriginalImage];
+    
+	// Get a file path to save the JPEG
+	NSArray* paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+	NSString* documentsDirectory = [paths objectAtIndex:0];
+	NSString* filename = [[self genRandStringLength:5] stringByAppendingString:@".jpg"];
+	NSString* imagePath = [documentsDirectory stringByAppendingPathComponent:filename];
+    
+	// Get the image data (blocking; around 1 second)
+	NSData* imageData = UIImageJPEGRepresentation(image, 0.5);
+    
+	// Write the data to the file
+	[imageData writeToFile:imagePath atomically:YES];
+    
+	// Tell the plugin class that we're finished processing the image
 	[self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:imagePath] callbackId:self.latestCommand.callbackId];
     
 	// Unset the self.hasPendingOperation property
 	self.hasPendingOperation = NO;
-    
+    AppDelegate *delegate = (AppDelegate*)[UIApplication sharedApplication].delegate;
 	// Hide the picker view
-	[self.viewController dismissModalViewControllerAnimated:YES];
+	[delegate.window.rootViewController dismissModalViewControllerAnimated:YES];
+}
+
+-(NSString *) genRandStringLength: (int) len {
+    
+    NSString *letters = @"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    
+    NSMutableString *randomString = [NSMutableString stringWithCapacity: len];
+    
+    for (int i=0; i<len; i++) {
+        [randomString appendFormat: @"%C", [letters characterAtIndex: arc4random() % [letters length]]];
+    }
+    
+    return randomString;
 }
 
 @end
