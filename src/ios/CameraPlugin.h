253df7c681f36ce3f06a81cd84df9f74cdd32117
Marcello Cardea <marcello.cardea@axant.it>
2014-03-18 15:38:24 +0100

    new changes

--- src/ios/CameraPlugin.h
+++ src/ios/CameraPlugin.h
@@ -1,22 +1,18 @@
-//
-//  CameraPlugin.h
-//
-//
-//  Created by Marci on 26/02/14.
-//
-//
-
+// Note that Xcode gets this line wrong.  You need to change "Cordova.h" to "CDV.h" as shown below.
 #import <Cordova/CDV.h>
-#import <objc/runtime.h>
 
-@interface CameraPlugin : CDVPlugin <UIAlertViewDelegate> {
-    Method originalHandleOpenUrl;
-    Method myHandleOpenUrl;
-}
+// Import the CustomCameraViewController class
+#import "CustomCameraViewController.h"
 
-@property (nonatomic, strong) NSString* callbackId;
+@interface CameraPlugin : CDVPlugin
 
-- (void)initPlugin:(CDVInvokedUrlCommand*)command;
-- (BOOL)application:(UIApplication *)application newHandleOpenUrlImpl:(NSURL *)url;
+// Cordova command method
+-(void) openCamera:(CDVInvokedUrlCommand*)command;
+
+// Create and override some properties and methods (these will be explained later)
+-(void) capturedImageWithPath:(NSString*)imagePath;
+@property (strong, nonatomic) CustomCameraViewController *overlay;
+@property (strong, nonatomic) CDVInvokedUrlCommand *latestCommand;
+@property (readwrite, assign) BOOL hasPendingOperation;
 
 @end
--- src/ios/CameraPlugin.m
+++ src/ios/CameraPlugin.m
@@ -1,103 +1,33 @@
-//
-//  CameraPlugin.m
-//
-//
-//  Created by Marc on 26/02/14.
-//
-//
-
 #import "CameraPlugin.h"
-
-NSString *appname = @"cameraplugin";
-static char CAMERA_PLUGIN_REF_KEY;
-static IMP UIApplicationDelegate_handleOpenURL_original;
 
 @implementation CameraPlugin
 
-- (void)initPlugin:(CDVInvokedUrlCommand*)command{
-    id appDelegate = [[UIApplication sharedApplication] delegate];
+// Cordova command method
+-(void) openCamera:(CDVInvokedUrlCommand *)command {
     
-    // Store a reference to the plugin so we can access it from our handleOpenUrl
-    objc_setAssociatedObject(appDelegate, &CAMERA_PLUGIN_REF_KEY, self, OBJC_ASSOCIATION_RETAIN);
+	// Set the hasPendingOperation field to prevent the webview from crashing
+	self.hasPendingOperation = YES;
     
-    // Store references to the handleOpenUrl methods so we can switch them on demand
-    self->myHandleOpenUrl = class_getInstanceMethod([self class], @selector(application:newHandleOpenUrlImpl:));
-    self->originalHandleOpenUrl = class_getInstanceMethod([appDelegate class], @selector(application:handleOpenURL:));
+	// Save the CDVInvokedUrlCommand as a property.  We will need it later.
+	self.latestCommand = command;
+    
+	// Make the overlay view controller.
+	self.overlay = [[CustomCameraViewController alloc] initWithNibName:@"CustomCameraViewController" bundle:nil];
+	self.overlay.plugin = self;
+    
+	// Display the view.  This will "slide up" a modal view from the bottom of the screen.
+	[self.viewController presentViewController:self.overlay.picker animated:YES completion:nil];
 }
 
--(BOOL) application:(UIApplication *)application newHandleOpenUrlImpl:(NSURL *)url {
-    CDVPluginResult* pluginResult = nil;
-    NSString *value = [url query];
-    NSLog(@"%@", url);
-
-    if (true) {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"paid"];
-
-    } else {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"failed"];
-    }
-
-    // self is a reference to AppDelegate
-    CameraPlugin *plugin = (CameraPlugin *)objc_getAssociatedObject(self, &CAMERA_PLUGIN_REF_KEY);
+// Method called by the overlay when the image is ready to be sent back to the web view
+-(void) capturedImageWithPath:(NSString*)imagePath {
+	[self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:imagePath] callbackId:self.latestCommand.callbackId];
     
-    // Replace handleOpenUrl with original application one
-    method_exchangeImplementations(plugin->myHandleOpenUrl, plugin->originalHandleOpenUrl);
+	// Unset the self.hasPendingOperation property
+	self.hasPendingOperation = NO;
     
-    // Send payment result
-    [plugin.commandDelegate sendPluginResult:pluginResult callbackId:plugin.callbackId];
-    return YES;
+	// Hide the picker view
+	[self.viewController dismissModalViewControllerAnimated:YES];
 }
-
-- (void)performPayment:(CDVInvokedUrlCommand*)command{
-
-    NSString* value = [command.arguments objectAtIndex:0];
-    BOOL isInstalled = [[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"IlmioPOS://"]];
-    CDVPluginResult* pluginResult = nil;
-    BOOL isValid = NO;
-    NSCharacterSet *alphaNumbersSet = [NSCharacterSet decimalDigitCharacterSet];
-    NSCharacterSet *stringSet = [NSCharacterSet characterSetWithCharactersInString:value];
-    isValid = [alphaNumbersSet isSupersetOfSet:stringSet];
-    if (isInstalled & isValid) {
-        self.callbackId = command.callbackId;
-        NSString *myString = [NSString stringWithFormat:@"IlmioPOS://?value=%@&sender=%@", value, appname];
-        NSURL *myURL = [NSURL URLWithString:myString];
-
-        // Replace application handleOpenUrl with our own version
-        method_exchangeImplementations(self->originalHandleOpenUrl, self->myHandleOpenUrl);
-
-        [[UIApplication sharedApplication] openURL:myURL];
-    } else {
-        if(!isValid)
-            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString: @"-5"];
-        else
-            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString: @"-1"];
-        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-    }
-}
-
-- (void)isAvailable:(CDVInvokedUrlCommand*)command {
-    CDVPluginResult* pluginResult = nil;
-
-    BOOL isInstalled = [[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"IlmioPOS://"]];
-    if (isInstalled) {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"available"];
-
-    } else {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString: @"-1"];
-    }
-    
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-}
-
-- (void)showConfirmAlert{
-    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Mobile Pos Not Found!"
-                                                        message:@"Mobile Pos Application is not installed on this device!"
-                                                       delegate:nil
-                                              cancelButtonTitle:@"Ok"
-                                              otherButtonTitles:nil];
-        [alert show];
-
-}
-
 
 @end
