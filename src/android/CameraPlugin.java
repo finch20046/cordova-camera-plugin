2966740dd1851749c720aeae5e1c70f5e0f9b5f9
Marcello Cardea <marcello.cardea@axant.it>
2014-03-19 15:40:39 +0100

    changes

--- src/android/CameraPlugin.java
+++ src/android/CameraPlugin.java
@@ -1,5 +1,8 @@
 package com.marcello.cameraplugin;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 import android.widget.Toast;
@@ -15,21 +18,22 @@ import android.util.Log;
 import android.widget.Toast;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Bundle;
 import android.provider.MediaStore;
+import android.provider.MediaStore.Images;
+
 
 public class CameraPlugin extends CordovaPlugin {
-
-	private final String INGENICO_PAKAGE = "com.ingenico.bancasella";
-	private final String INGENICO_SUB_PAKAGE = "com.ingenico.pos";
-	private final int INGENICO_PAYMENT = 500;
+	
+	static int TAKE_PICTURE = 1;
 	private Intent intent;
-	private final String TAG = "SCLOBY";
-	private CallbackContext paymentCallbackContext;
+	private CallbackContext callbackContext;
 
 	@Override
-	public boolean execute(String action, JSONArray args,
-			CallbackContext callbackContext) throws JSONException {
+	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
 		System.out.println("============>" + action);
 
 		if (action.equals("initPlugin")) {
@@ -46,30 +50,43 @@ public class CameraPlugin extends CordovaPlugin {
 	}
 
 	private void openCamera(CallbackContext callbackContext) {
-	    this.paymentCallbackContext = callbackContext;
-
+	    this.callbackContext = callbackContext;
+	    //Context context = cordova.getActivity().getApplicationContext();
+	    //Intent intent = new Intent(context, CameraPlugin.class);
         // create Intent to take a picture and return control to the calling application
-     // create intent with ACTION_IMAGE_CAPTURE action
+        //create intent with ACTION_IMAGE_CAPTURE action
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
+        this.cordova.setActivityResultCallback(CameraPlugin.this);
         // start camera activity
         this.cordova.getActivity().startActivityForResult(intent, 1);
-
+        
 	}
+	
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+		if ((requestCode == TAKE_PICTURE) && (resultCode == Activity.RESULT_OK)) {
+	        Uri photoPath = intent.getData();
+	                    // do something with the uri here
+	        String uri = getPath(photoPath);
+	        Log.d("CAMERAPLUGIN", uri);
+	        this.callbackContext.success(uri);
+	    }else
+	    	this.callbackContext.success("error");
+	}
+	
+	private String getPath(Uri uri) {
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+	    String[] projection = { MediaStore.Images.Media.DATA };
+	    Cursor cursor = this.cordova.getActivity().getContentResolver().query(uri, projection, null, null,null);
 
-        if (requestCode == TAKE_PICTURE && resultCode== RESULT_OK && intent != null){
-            // get bundle
-            Bundle extras = intent.getExtras();
+	    int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+	    cursor.moveToFirst();
 
-            // get bitmap
-            bitMap = (Bitmap) extras.get("data");
-            ivThumbnailPhoto.setImageBitmap(bitMap);
-
-        }
-    }
+	    return cursor.getString(column_index);
+	}
+	
+	
+	
 
 	private void initPlugin(CallbackContext callbackContext) {
 
